df19b32d951d384e12e14826dcda700b19c3dfe6
Akash Kumar <akashkumar59200@gmail.com>
2022-05-31 11:31:23 +0530

    Angular Projects

---
+++ AngularProjects/angular-front-end/src/app/components/add-tutorial/add-tutorial.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Tutorial } from 'src/app/models/tutorials';
+import { TutorialService } from 'src/app/services/tutorial.service';
+
+@Component({
+  selector: 'app-add-tutorial',
+  templateUrl: './add-tutorial.component.html',
+  styleUrls: ['./add-tutorial.component.css']
+})
+export class AddTutorialComponent implements OnInit {
+  tutorial: Tutorial = {
+    id: undefined,
+    title:'',
+    description: '',
+    published: false
+  };
+  submitted = false;
+
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+  }
+
+  saveTutorial(): void {
+    console.log(this.submitted)
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description
+    };
+
+    this.tutorialService.create(data)
+      .subscribe(
+        (response: any) => {
+          console.log(response);
+          this.submitted = true;
+        },
+        (error: any) => {
+          console.log(error);
+        });
+  }
+
+  newTutorial(): void {
+    console.log(this.submitted)
+    this.submitted = false;
+    console.log(this.submitted)
+    this.tutorial = {
+      id: undefined,
+      title: '',
+      description: '',
+      published: false
+    };
+  }
+
+}
